7a77d997e69631a0652a7c9b01d329d40a33ac63
eddieh310 <ejherrer@buffalo.edu>
2025-06-03 13:02:15 -0400

    done for now

--- main.tf
+++ main.tf
@@ -7,11 +7,35 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-east-1" # or your preferred region
+}
+
 resource "aws_s3_bucket" "internal_bucket_name" {
-  bucket = var.s3_bucket_name
+  bucket              = var.s3_bucket_name
+  object_lock_enabled = true
 
   tags = {
     Name        = var.s3_bucket_name
     Environment = "Test"
+    CreatedBy   = "Terraform"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "example_lifecycle_config" {
+  bucket = aws_s3_bucket.internal_bucket_name.id
+
+  rule {
+    id     = "example-object-transition"
+    status = "Enabled"
+
+    filter {
+      prefix = "this/is/a/fake/prefix"
+    }
+
+    transition {
+      days          = 365
+      storage_class = "STANDARD_IA"
+    }
   }
 }
